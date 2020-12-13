862ad8d60bbe78761e10c7f4a5696eeb1113d7a1
root <18842920976@163.com>
2020-12-13 19:27:16 +0800

    ionic init

---
+++ src/app/tab1/tab2.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pdf',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class PDFViewerApplication {
+
+  constructor() {}
+
+}
